23626fa2dc47275607182dc7acdc773e2afed327
Никита Припачкин <npripachkin@mail.ru>
2024-07-16 18:58:04 +0300

    fill in Dto

--- back/src/main/java/ru/finam/backend/model/dto/FirmaDto.java
+++ back/src/main/java/ru/finam/backend/model/dto/FirmaDto.java
@@ -1,4 +1,9 @@
 package ru.finam.backend.model.dto;
 
 public class FirmaDto {
+    private String tickerName;
+    private String name;
+    private float price;
+    private float capitalization;
+    private float averageTradingVolume;
 }
--- back/src/main/java/ru/finam/backend/model/dto/InputDto.java
+++ back/src/main/java/ru/finam/backend/model/dto/InputDto.java
@@ -1,4 +1,13 @@
 package ru.finam.backend.model.dto;
 
 public class InputDto {
+    private String tickername;
+    private String type;
+    private String sector;
+    private float priceFrom;
+    private float priceUpTo;
+    private float capitalizationFrom;
+    private float capitalizationUpTo;
+    private float volumeFrom;
+    private float volumeUpTo;
 }
