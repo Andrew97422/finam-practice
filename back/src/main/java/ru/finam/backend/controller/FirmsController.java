37171f944269486ca4cea87552f7e035a39b1e4d
DayLips <alldaylips@yandex.ru>
2024-07-16 14:56:15 +0300

    Написал макеты основных функций для контроллера

--- back/src/main/java/ru/finam/backend/controller/FirmsController.java
+++ back/src/main/java/ru/finam/backend/controller/FirmsController.java
@@ -1,9 +1,35 @@
 package ru.finam.backend.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.finam.backend.model.dto.InputDto;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/firms")
 public class FirmsController {
+    @GetMapping
+    public List<InputDto> findAllInputDto() {
+        return null;
+    }
+
+    @PostMapping("save_input_dto")
+    public InputDto saveInputDto(@RequestBody InputDto inputdto) {
+        return null;
+    }
+
+    @GetMapping("/{tiker}")
+    public InputDto findByTiker(@PathVariable String tiker) {
+        return null;
+    }
+
+    @PutMapping("update_input_dto")
+    public InputDto updateInputDto(InputDto inputdto) {
+        return null;
+    }
+
+    @DeleteMapping("delete_input_dto/{tiker}")
+    public void deleteInputDto(@PathVariable String tiker) {
+
+    }
 }
