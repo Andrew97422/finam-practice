5aadf9b89ec2f2883b7f28ee6e8ae8e0c2f7927f
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-07-16 18:21:48 +0300

    fix

--- front/src/components/FilterForm.jsx
+++ front/src/components/FilterForm.jsx
@@ -17,7 +17,7 @@ const FilterForm = ({ onFilterChange }) => {
 			capitalizationFrom: "0",
 			capitalizationUpTo: "7000000000000",
 			volumeFrom: "0",
-			volumeUpTo: "1	",
+			volumeUpTo: "1",
 		},
 	});
 
