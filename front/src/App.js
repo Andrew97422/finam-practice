df2617c6ab9cad424ac18a4a7551c21f2fe4a517
IvanOrlovsky <iiiaaaiii706@gmail.com>
2024-07-16 13:44:36 +0300

    test

--- front/src/App.js
+++ front/src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import SideBar from "./components/SideBar";
 import { fetchData } from "./services/api";
 import "./index.css";
+import DataTable from "./components/DataTable";
 
 function App() {
 	const [data, setData] = useState([]);
@@ -27,9 +28,21 @@ function App() {
 	};
 
 	return (
-		<div>
-			<p>{JSON.stringify(data)}</p>
-			<SideBar onFilterChange={handleFilterChange} />
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
 		</div>
 	);
 }
