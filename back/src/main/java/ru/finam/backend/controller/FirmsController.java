45552ec6c8c4d8edc8fc204d58d53cb8a3ef5038
DayLips <alldaylips@yandex.ru>
2024-07-17 14:47:53 +0300

    Исправил контроллер для вас ребята

--- back/src/main/java/ru/finam/backend/controller/FirmsController.java
+++ back/src/main/java/ru/finam/backend/controller/FirmsController.java
@@ -1,7 +1,10 @@
 package ru.finam.backend.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import ru.finam.backend.model.dto.FinanceInstrumentRequestDTO;
+import ru.finam.backend.service.ApplicationUtils;
 import ru.finam.backend.service.FirmFilterService;
 
 import java.util.List;
@@ -12,25 +15,26 @@ import java.util.List;
 @RequestMapping("/api/v1/firms")
 public class FirmsController {
 
-    private FirmFilterService firmFilterService;
+    private final FirmFilterService firmFilterService;
+    private final ApplicationUtils applicationUtils;
 
-    @GetMapping
-    public List<InputDto> findAllInputDto() {
-        return null;
+    @GetMapping("/")
+    public String financeInstrumentRequestDTO(Model model) {
+        return "financeInstrumentRequestDTO";
     }
 
     @PostMapping("save_input_dto")
-    public InputDto saveInputDto(@RequestBody InputDto inputdto) {
+    public FinanceInstrumentRequestDTO saveInputDto(@RequestBody FinanceInstrumentRequestDTO inputdto) {
         return null;
     }
 
     @GetMapping("/{tiker}")
-    public InputDto findByTiker(@PathVariable String tiker) {
+    public FinanceInstrumentRequestDTO findByTiker(@PathVariable String tiker) {
         return null;
     }
 
     @PutMapping("update_input_dto")
-    public InputDto updateInputDto(InputDto inputdto) {
+    public FinanceInstrumentRequestDTO updateInputDto(FinanceInstrumentRequestDTO inputdto) {
         return null;
     }
 
