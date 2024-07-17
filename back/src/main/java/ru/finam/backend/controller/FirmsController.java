5e4572f31dddd1a0c3c9a04f48f06635bbeb7a76
DayLips <alldaylips@yandex.ru>
2024-07-17 22:54:38 +0300

    Дописал контроллер и документцаию

--- back/src/main/java/ru/finam/backend/controller/FirmsController.java
+++ back/src/main/java/ru/finam/backend/controller/FirmsController.java
@@ -4,16 +4,15 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.finam.backend.model.dto.FinanceInstrumentRequestDTO;
 import ru.finam.backend.model.dto.FinanceInstrumentResponseDTO;
-import ru.finam.backend.service.ApplicationUtils;
 import ru.finam.backend.service.FinanceInstrumentService;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -25,10 +24,11 @@ import ru.finam.backend.service.FinanceInstrumentService;
 @Slf4j
 public class FirmsController {
 
-    private final FinanceInstrumentService financeInstrumentService;
+    private final    FinanceInstrumentService financeInstrumentService;
 
     @Operation(
-            summary = "Получение инструментов"
+            summary = "Получение инструментов",
+            description = "Получения инструментов по фильтрам"
     )
     @GetMapping("/finance_instruments")
     public ResponseEntity<Page<FinanceInstrumentResponseDTO>> getFinanceInstruments(
@@ -45,25 +45,5 @@ public class FirmsController {
             log.error("Instrument {} was not found", filter);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-    }
-
-    @PostMapping("save_input_dto")
-    public FinanceInstrumentRequestDTO saveInputDto(@RequestBody FinanceInstrumentRequestDTO inputdto) {
-        return null;
-    }
-
-    @GetMapping("/{tiker}")
-    public FinanceInstrumentRequestDTO findByTiker(@PathVariable String tiker) {
-        return null;
-    }
-
-    @PutMapping("update_input_dto")
-    public FinanceInstrumentRequestDTO updateInputDto(FinanceInstrumentRequestDTO inputdto) {
-        return null;
-    }
-
-    @DeleteMapping("delete_input_dto/{tiker}")
-    public void deleteInputDto(@PathVariable String tiker) {
-
     }
 }
