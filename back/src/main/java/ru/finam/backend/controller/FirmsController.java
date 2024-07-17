97a9856112398b158f21a2a6e470ffc6f966fbfd
DayLips <alldaylips@yandex.ru>
2024-07-17 19:41:17 +0300

    Я написал метод для getFinanceInstruments, через ResponseEntity

--- back/src/main/java/ru/finam/backend/controller/FirmsController.java
+++ back/src/main/java/ru/finam/backend/controller/FirmsController.java
@@ -1,9 +1,14 @@
 package ru.finam.backend.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.ui.Model;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.finam.backend.model.dto.FinanceInstrumentRequestDTO;
+import ru.finam.backend.model.dto.FinanceInstrumentResponseDTO;
 import ru.finam.backend.service.ApplicationUtils;
 import ru.finam.backend.service.FinanceInstrumentService;
 
@@ -13,12 +18,24 @@ import ru.finam.backend.service.FinanceInstrumentService;
 @RequestMapping("/api/v1/firms")
 public class FirmsController {
 
+    private static final Logger log = LoggerFactory.getLogger(FirmsController.class);
     private final FinanceInstrumentService financeInstrumentService;
-    private final ApplicationUtils applicationUtils;
 
-    @GetMapping("/")
-    public String financeInstrumentRequestDTO(Model model) {
-        return "financeInstrumentRequestDTO";
+    @GetMapping("/finance_instruments")
+    public ResponseEntity<Page<FinanceInstrumentResponseDTO>> getFinanceInstruments(
+            @PathVariable FinanceInstrumentRequestDTO filter,
+            @PathVariable int offset,
+            @PathVariable int limit
+
+    ) {
+        try {
+            Page<FinanceInstrumentResponseDTO> response = financeInstrumentService.getFinanceInstruments(filter, offset, limit);
+            log.info("Instrument {} was found", filter);
+            return ResponseEntity.ok(response);
+        } catch (IllegalAccessError e) {
+            log.error("Instrument {} was not found", filter);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @PostMapping("save_input_dto")
