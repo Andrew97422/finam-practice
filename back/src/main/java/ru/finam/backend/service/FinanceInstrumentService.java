a875ada81099fc8028730cb8985cf2db74f98372
yevgn <zhenya041010@gmail.com>
2024-07-18 20:27:54 +0500

    Исправлена ошибка в запросе в CustomFinanceInstrumentRepositoryImpl

--- back/src/main/java/ru/finam/backend/service/FinanceInstrumentService.java
+++ back/src/main/java/ru/finam/backend/service/FinanceInstrumentService.java
@@ -1,36 +1,37 @@
 package ru.finam.backend.service;
 
+
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 import ru.finam.backend.model.dto.FinanceInstrumentRequestDTO;
 import ru.finam.backend.model.dto.FinanceInstrumentResponseDTO;
 import ru.finam.backend.model.entities.FinanceInstrumentEntity;
 import ru.finam.backend.repositories.FinanceInstrumentRepository;
 
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 public class FinanceInstrumentService {
 
-  private final FinanceInstrumentRepository financeInstrumentRepository;
-  private final ApplicationUtils applicationUtils;
+    private final FinanceInstrumentRepository financeInstrumentRepository;
+    private final ApplicationUtils  applicationUtils;
 
-  public Page<FinanceInstrumentResponseDTO> getFinanceInstruments(
-      FinanceInstrumentRequestDTO filter, int offset, int limit) {
+    public Page<FinanceInstrumentResponseDTO> getFinanceInstruments(
+        FinanceInstrumentRequestDTO filter, int offset, int limit) {
 
-      // Получение списка отфильтрованных сущностей из БД
-      List<FinanceInstrumentEntity> entitylist =
-          financeInstrumentRepository.findFinanceInstrumentsByFilter(filter);
+        // Получение списка отфильтрованных сущностей из БД
+        List<FinanceInstrumentEntity> entitylist =
+            financeInstrumentRepository.findFinanceInstrumentsByFilter(filter);
 
-      // Преобразование списка сущностей в список responseDTO
-      List<FinanceInstrumentResponseDTO> responseDTOList =
-          applicationUtils.mapToFinanceInstrumentResponseDTOList(entitylist);
+        // Преобразование списка сущностей в список responseDTO
+        List<FinanceInstrumentResponseDTO> responseDTOList =
+            applicationUtils.mapToFinanceInstrumentResponseDTOList(entitylist);
 
-      // перевод в Page<FinanceInstrumentResponseDTO> и возвращение
-      return applicationUtils.convertListToPage(responseDTOList, offset, limit);
-  }
-
+        // перевод в Page<FinanceInstrumentResponseDTO> и возвращение
+        return applicationUtils.convertListToPage(responseDTOList, offset, limit);
+    }
 }
