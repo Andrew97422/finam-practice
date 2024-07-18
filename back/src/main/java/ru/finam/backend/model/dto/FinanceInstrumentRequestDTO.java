498e0753eb2ba01d525d20466943910023a8c61c
DayLips <alldaylips@yandex.ru>
2024-07-18 15:10:14 +0300

    Добавмл в DTO файлы документацию для свагера

--- back/src/main/java/ru/finam/backend/model/dto/FinanceInstrumentRequestDTO.java
+++ back/src/main/java/ru/finam/backend/model/dto/FinanceInstrumentRequestDTO.java
@@ -1,5 +1,6 @@
 package ru.finam.backend.model.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,14 +10,34 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(name = "Фильтры", description = "Выбирается пользователем")
 public class FinanceInstrumentRequestDTO {
+
+    @Schema(description = "Наименование", name = "tickerName")
     private String tickerName;
+
+    @Schema(description = "Тип инструмента", name = "type")
     private String type;
+
+    @Schema(description = "Сектор", name = "sector")
     private String sector;
+
+    @Schema(description = "Цена от", name = "priceFrom")
     private String priceFrom;
+
+    @Schema(description = "Цена до", name = "priceUpTo")
     private String priceUpTo;
+
+    @Schema(description = "Капитализация от", name = "capitalizationFrom")
     private String capitalizationFrom;
+
+    @Schema(description = "Капитализация до", name = "capitalizationUpTo")
     private String capitalizationUpTo;
+
+    @Schema(description = "Средний объем торгов от", name = "volumeFrom")
     private String volumeFrom;
+
+    @Schema(description = "Средний объем торгов до", name = "volumeUpTo")
     private String volumeUpTo;
+
 }
