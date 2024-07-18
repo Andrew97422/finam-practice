ed23d5ac1fcf4e347d9334bb8b9ef5fb97dac3b0
yevgn <zhenya041010@gmail.com>
2024-07-18 18:37:17 +0500

    изменен способ получения данных в CustomFinanceInstrumentRepositoryImpl

--- back/src/main/java/ru/finam/backend/repositories/customrepositories/financeinstrumentrepository/CustomFinanceInstrumentRepository.java
+++ back/src/main/java/ru/finam/backend/repositories/customrepositories/financeinstrumentrepository/CustomFinanceInstrumentRepository.java
@@ -1,10 +1,13 @@
 package ru.finam.backend.repositories.customrepositories.financeinstrumentrepository;
 
 import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.finam.backend.model.dto.FinanceInstrumentRequestDTO;
 import ru.finam.backend.model.entities.FinanceInstrumentEntity;
 
 public interface CustomFinanceInstrumentRepository {
-      List<FinanceInstrumentEntity> findFinanceInstrumentsByFilter(
-          FinanceInstrumentRequestDTO filter);
+
+        List<FinanceInstrumentEntity> findFinanceInstrumentsByFilter(
+             FinanceInstrumentRequestDTO filter);
 }
--- back/src/main/java/ru/finam/backend/repositories/customrepositories/financeinstrumentrepository/CustomFinanceInstrumentRepositoryImpl.java
+++ back/src/main/java/ru/finam/backend/repositories/customrepositories/financeinstrumentrepository/CustomFinanceInstrumentRepositoryImpl.java
@@ -2,6 +2,8 @@ package ru.finam.backend.repositories.customrepositories.financeinstrumentreposi
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
 import java.util.List;
 import org.springframework.stereotype.Repository;
 import ru.finam.backend.model.dto.FinanceInstrumentRequestDTO;
@@ -11,38 +13,37 @@ import ru.finam.backend.model.entities.FinanceInstrumentEntity;
 public class CustomFinanceInstrumentRepositoryImpl implements CustomFinanceInstrumentRepository {
 
     @PersistenceContext
-    private EntityManager entityManager;
+    private EntityManager em;
 
     @Override
     public List<FinanceInstrumentEntity> findFinanceInstrumentsByFilter(
         FinanceInstrumentRequestDTO dto) {
-
-        String baseQuery =
-            "FROM finance_instruments fi JOIN firms f ON f.firm_id = fi.firm_id"
+        String filterQuery =
+            "SELECT * FROM finance_instruments fi JOIN firms f ON f.firm_id = fi.firm_id"
                 + " JOIN sectors s ON s.sector_id = f.sector_id JOIN instrument_types it ON"
                 + " it.instrument_type_id = fi.instrument_type_id WHERE";
 
         if (!dto.getTickerName().isEmpty()) {
-            baseQuery += " f.ticker LIKE '%" + dto.getTickerName() + "%' OR "
+            filterQuery += " f.ticker LIKE '%" + dto.getTickerName() + "%' OR "
                 + " f.name LIKE '%" + dto.getTickerName() + "%' AND";
         }
 
-        baseQuery += " s.type_of_sector LIKE '" + dto.getSector() + "' AND";
+        filterQuery += " s.type_of_sector LIKE '" + dto.getSector() + "' AND";
 
-        baseQuery += " it.instrument_type LIKE '" + dto.getType() + "' AND";
+        filterQuery += " it.instrument_type LIKE '" + dto.getType() + "' AND";
 
-        baseQuery += " fi.price BETWEEN "
+        filterQuery += " fi.price BETWEEN "
             + dto.getPriceFrom() + " AND " +
             dto.getPriceUpTo() + " AND";
 
-        baseQuery += " f.capitalization BETWEEN "
+        filterQuery += " f.capitalization BETWEEN "
             + dto.getCapitalizationFrom() + " AND " +
             dto.getCapitalizationUpTo() + " AND";
 
-        baseQuery += " fi.average_trading_volume BETWEEN "
+        filterQuery += " fi.average_trading_volume BETWEEN "
             + dto.getVolumeFrom() + " AND " +
             dto.getVolumeUpTo();
 
-        return entityManager.createQuery(baseQuery, FinanceInstrumentEntity.class).getResultList();
+        return em.createNativeQuery(filterQuery, FinanceInstrumentEntity.class).getResultList();
     }
 }
