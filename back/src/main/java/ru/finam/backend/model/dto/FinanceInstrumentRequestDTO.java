3e803dc0f7ccac594cd767b4fced471e95019e53
Никита Припачкин <npripachkin@mail.ru>
2024-07-19 14:39:51 +0300

    добавление валидации в dto

--- back/src/main/java/ru/finam/backend/model/dto/FinanceInstrumentRequestDTO.java
+++ back/src/main/java/ru/finam/backend/model/dto/FinanceInstrumentRequestDTO.java
@@ -1,6 +1,8 @@
 package ru.finam.backend.model.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,31 +15,46 @@ import lombok.NoArgsConstructor;
 @Schema(name = "Фильтры", description = "Выбирается пользователем")
 public class FinanceInstrumentRequestDTO {
 
+    @NotNull
     @Schema(description = "Наименование", name = "tickerName", example = "SBER")
     private String tickerName;
 
+    @NotNull
     @Schema(description = "Тип инструмента", name = "type", example = "Акции")
     private String type;
 
+    @NotNull
     @Schema(description = "Сектор", name = "sector", example = "Финансы")
     private String sector;
 
+    @PositiveOrZero
+    @Digits(integer = 30,fraction = 8)
     @Schema(description = "Цена от", name = "priceFrom", example = "0")
-    private String priceFrom;
+    private float priceFrom;
 
+    @Positive
+    @Digits(integer = 30,fraction = 8)
     @Schema(description = "Цена до", name = "priceUpTo", example = "300000")
-    private String priceUpTo;
+    private float priceUpTo;
 
+    @PositiveOrZero
+    @Digits(integer = 30,fraction = 8)
     @Schema(description = "Капитализация от", name = "capitalizationFrom", example = "0")
-    private String capitalizationFrom;
+    private float capitalizationFrom;
 
+    @Positive
+    @Digits(integer = 30,fraction = 8)
     @Schema(description = "Капитализация до", name = "capitalizationUpTo", example = "7000000000000")
-    private String capitalizationUpTo;
+    private float capitalizationUpTo;
 
+    @PositiveOrZero
+    @Digits(integer = 30,fraction = 8)
     @Schema(description = "Средний объем торгов от", name = "volumeFrom", example = "0")
-    private String volumeFrom;
+    private float volumeFrom;
 
+    @Positive
+    @Digits(integer = 30,fraction = 8)
     @Schema(description = "Средний объем торгов до", name = "volumeUpTo", example = "1")
-    private String volumeUpTo;
+    private float volumeUpTo;
 
 }
