b04e632a03a63d317b4c36840370574cd136a2b0
DayLips <alldaylips@yandex.ru>
2024-07-19 17:17:56 +0300

    Добавил описание ошибок и валидацию на контроллере

--- back/src/main/java/ru/finam/backend/controller/FinanceInstrumentController.java
+++ back/src/main/java/ru/finam/backend/controller/FinanceInstrumentController.java
@@ -2,11 +2,17 @@ package ru.finam.backend.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -31,11 +37,25 @@ public class FinanceInstrumentController {
             summary = "Получение инструментов",
             description = "Получения инструментов по фильтрам"
     )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "Данной страницы не существует",
+                    content = @Content(mediaType = "application/json", examples = { @ExampleObject( value = "{\"name\": \"pong\", \"description\":\"test\"}") })),
+            @ApiResponse(responseCode = "400", description = "Неправильное значение числа элементов страницы",
+                    content = @Content(mediaType = "application/json", examples = { @ExampleObject( value = "{\"name\": \"pong\", \"description\":\"test\"}") })),
+            @ApiResponse(responseCode = "200", description = "ОК",
+                    content = @Content(mediaType = "application/json", examples = { @ExampleObject( value = "{\"name\": \"pong\", \"description\":\"test\"}") }))
+    })
     @PostMapping("/finance_instruments/{offset}/{limit}")
     public ResponseEntity<Page<FinanceInstrumentResponseDTO>> getFinanceInstruments(
-            @RequestBody FinanceInstrumentRequestDTO filter,
-            @PathVariable @Parameter(description = "Смещение") int offset,
-            @PathVariable @Parameter(description = "Количество записей") int limit
+            @Valid @RequestBody FinanceInstrumentRequestDTO filter,
+
+            @PathVariable @Parameter(description = "Смещение")
+            @Min(0) @Max(Integer.MAX_VALUE)
+            @RequestParam(required = true, defaultValue = "0", name = "offset") int offset,
+
+            @PathVariable @Parameter(description = "Количество записей")
+            @Min(0) @Max(100)
+            @RequestParam(required = true, defaultValue = "0", name = "limit") int limit
 
     ) {
         try {
--- back/src/main/java/ru/finam/backend/model/dto/FinanceInstrumentResponseDTO.java
+++ back/src/main/java/ru/finam/backend/model/dto/FinanceInstrumentResponseDTO.java
@@ -3,7 +3,6 @@ package ru.finam.backend.model.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
