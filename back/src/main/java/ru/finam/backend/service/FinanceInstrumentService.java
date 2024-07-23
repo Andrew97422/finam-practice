2d3a0630de948000f243b7f12fc6b24f2419cb5c
yevgn <zhenya041010@gmail.com>
2024-07-23 22:41:53 +0500

    Добавлена реализация доступа к кэшу в FinanceInstrumentService

--- back/src/main/java/ru/finam/backend/service/FinanceInstrumentService.java
+++ back/src/main/java/ru/finam/backend/service/FinanceInstrumentService.java
@@ -4,22 +4,22 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.data.domain.Page;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import ru.finam.backend.model.dto.FinanceInstrumentRequestDTO;
 import ru.finam.backend.model.dto.FinanceInstrumentResponseDTO;
 import ru.finam.backend.model.entities.FinanceInstrumentEntity;
 
-import ru.finam.backend.repositories.FinanceInstrumentRepository;
 import ru.finam.backend.validation.ValidationService;
 
 @Service
@@ -28,9 +28,12 @@ public class FinanceInstrumentService {
 
     private final ApplicationUtils applicationUtils;
     private final ValidationService validationService;
-    private final FinanceInstrumentRepository fiRepository;
-
+    private final RedisTemplate<String, Object> redisTemplate;
     @PersistenceContext
+    private final EntityManager em;
+
+    private static final String KEY = "INSTRUMENTS";
+
 
     public Page<FinanceInstrumentResponseDTO> getFinanceInstruments(
             FinanceInstrumentRequestDTO filter, int offset, int limit) throws IndexOutOfBoundsException,
@@ -38,12 +41,8 @@ public class FinanceInstrumentService {
         // проверка полей DTO на валидность
         validationService.checkRequestDTOFieldsAreValid(filter);
 
-        // Получение отфильтрованных данных с помощью CriteriaQuery
-        List<FinanceInstrumentEntity> entitylist = getFilteredInstrumentsFromDB(filter);
-
-
-
-
+        // Получение отфильтрованных данных либо из кэша, либо из БД
+        List<FinanceInstrumentEntity> entitylist = getInstruments(filter);
 
         // проверка offset и limit на валидность
         validationService.checkOffsetAndLimitAreValid(offset, limit, entitylist.size());
@@ -56,11 +55,86 @@ public class FinanceInstrumentService {
         return applicationUtils.convertListToPage(responseDTOList, offset, limit);
     }
 
-    private List<FinanceInstrumentEntity> getFilteredInstruments(FinanceInstrumentRequestDTO filter){
-       if(!filter.getTickerName().isEmpty()){
-           //А тикернейм в кеше??
+    private List<FinanceInstrumentEntity> getInstruments(FinanceInstrumentRequestDTO filter){
+        String tickerName = filter.getTickerName();
+        List<FinanceInstrumentEntity> entityList;
 
-       }
+        if (redisTemplate.opsForHash().hasKey(KEY, tickerName)) {
+            entityList = (List<FinanceInstrumentEntity>) redisTemplate.opsForHash().get(KEY, tickerName);
+        } else{
+            entityList = getInstrumentsByTickerOrNameFromDB(tickerName);
+        }
+
+        List<FinanceInstrumentEntity> filteredEntityList = filterInstruments(filter, entityList);
+
+        if(!tickerName.isEmpty() && !redisTemplate.opsForHash().hasKey(KEY, tickerName)){
+            redisTemplate.opsForHash().put(KEY, tickerName, filteredEntityList);
+        }
+
+        return sortInstruments(filter.getSortBy(), filter.getSortOrder(), filteredEntityList);
     }
 
+    private List<FinanceInstrumentEntity> getInstrumentsByTickerOrNameFromDB(String tickerName){
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<FinanceInstrumentEntity> cr =
+                cb.createQuery(FinanceInstrumentEntity.class);
+        Root<FinanceInstrumentEntity> root =
+                cr.from(FinanceInstrumentEntity.class);
+
+        root.fetch("firm").fetch("sector");
+        root.fetch("instrumentType");
+
+        if (!tickerName.isEmpty()) {
+            cr.where(cb.or(
+                    cb.like(root.get("firm").get("ticker"), "%" + tickerName + "%"),
+                    cb.like(root.get("firm").get("name"), "%" + tickerName + "%")
+            ));
+        }
+
+        cr.select(root);
+
+        return em.createQuery(cr).getResultList();
+
+    }
+
+    private List<FinanceInstrumentEntity> filterInstruments(FinanceInstrumentRequestDTO filter,
+                                                            List<FinanceInstrumentEntity> l){
+
+        Predicate<FinanceInstrumentEntity> filterPredicate = fi -> {
+            return fi.getFirm().getSector().getName().equals(filter.getSector()) &&
+                    fi.getInstrumentType().getName().equals(filter.getType()) &&
+                    applicationUtils.isInRange(fi.getPrice(), filter.getPriceFrom(), filter.getPriceUpTo()) &&
+                    applicationUtils.isInRange(fi.getAverageTradingVolume(), filter.getVolumeFrom(),
+                            filter.getVolumeUpTo()) &&
+                    applicationUtils.isInRange(fi.getFirm().getCapitalization(), filter.getCapitalizationFrom(),
+                            filter.getCapitalizationUpTo());
+        };
+
+        return l.parallelStream().filter(filterPredicate).collect(Collectors.toList());
+    }
+
+    private List<FinanceInstrumentEntity> sortInstruments(String sortBy, String sortOrder,
+                                                            List<FinanceInstrumentEntity> l){
+        List<FinanceInstrumentEntity> sortedEntityList = new ArrayList<>();
+
+        switch (sortBy) {
+            case "price" -> sortedEntityList = l.parallelStream().sorted((fi1, fi2) -> {
+                return sortOrder.equals("asc") ? Float.compare(fi1.getPrice(), fi2.getPrice()) :
+                        Float.compare(fi2.getPrice(), fi1.getPrice());
+            }).toList();
+            case "averageTradingVolume" -> sortedEntityList = l.parallelStream().sorted((fi1, fi2) -> {
+                return sortOrder.equals("asc") ?
+                        Float.compare(fi1.getAverageTradingVolume(), fi2.getAverageTradingVolume()) :
+                        Float.compare(fi2.getAverageTradingVolume(), fi1.getAverageTradingVolume());
+            }).toList();
+            case "capitalization" -> sortedEntityList = l.parallelStream().sorted((fi1, fi2) -> {
+                return sortOrder.equals("asc") ?
+                        Float.compare(fi1.getFirm().getCapitalization(), fi2.getFirm().getCapitalization()) :
+                        Float.compare(fi2.getFirm().getCapitalization(), fi1.getFirm().getCapitalization());
+            }).toList();
+            default -> throw new IllegalArgumentException("Данного поля для сортировки не существует : " + sortBy);
+        }
+
+        return sortedEntityList;
+    }
 }
